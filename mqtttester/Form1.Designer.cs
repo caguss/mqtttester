00073d26e80c8dbd11bf77c224911d2e69839330
방찬석 <dipsy1014@naver.com>
2024-08-12 14:09:50 +0900

    초기 커밋.

---
+++ mqtttester/Form1.Designer.cs
@@ -0,0 +1,189 @@
+﻿namespace mqtttester
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.txtSendText = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.logbox = new System.Windows.Forms.ListBox();
+            this.txt_Action = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtIP = new System.Windows.Forms.TextBox();
+            this.txtPort = new System.Windows.Forms.TextBox();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 4;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 100F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 100F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Controls.Add(this.txtSendText, 1, 1);
+            this.tableLayoutPanel1.Controls.Add(this.label5, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.label3, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.logbox, 0, 3);
+            this.tableLayoutPanel1.Controls.Add(this.txt_Action, 0, 2);
+            this.tableLayoutPanel1.Controls.Add(this.label2, 2, 0);
+            this.tableLayoutPanel1.Controls.Add(this.txtIP, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.txtPort, 3, 0);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Margin = new System.Windows.Forms.Padding(0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 4;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 40F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 40F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 50F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(652, 410);
+            this.tableLayoutPanel1.TabIndex = 3;
+            // 
+            // txtSendText
+            // 
+            this.tableLayoutPanel1.SetColumnSpan(this.txtSendText, 3);
+            this.txtSendText.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.txtSendText.Font = new System.Drawing.Font("굴림", 16F);
+            this.txtSendText.Location = new System.Drawing.Point(103, 43);
+            this.txtSendText.Name = "txtSendText";
+            this.txtSendText.Size = new System.Drawing.Size(546, 32);
+            this.txtSendText.TabIndex = 10;
+            this.txtSendText.Text = "Hello";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label5.Location = new System.Drawing.Point(3, 40);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(94, 40);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "SendText :";
+            this.label5.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label3.Location = new System.Drawing.Point(3, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(94, 40);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "IP :";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // logbox
+            // 
+            this.tableLayoutPanel1.SetColumnSpan(this.logbox, 4);
+            this.logbox.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.logbox.FormattingEnabled = true;
+            this.logbox.ItemHeight = 12;
+            this.logbox.Location = new System.Drawing.Point(3, 133);
+            this.logbox.Name = "logbox";
+            this.logbox.Size = new System.Drawing.Size(646, 274);
+            this.logbox.TabIndex = 1;
+            // 
+            // txt_Action
+            // 
+            this.tableLayoutPanel1.SetColumnSpan(this.txt_Action, 4);
+            this.txt_Action.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.txt_Action.Font = new System.Drawing.Font("굴림", 14F);
+            this.txt_Action.Location = new System.Drawing.Point(3, 83);
+            this.txt_Action.Name = "txt_Action";
+            this.txt_Action.Size = new System.Drawing.Size(646, 44);
+            this.txt_Action.TabIndex = 2;
+            this.txt_Action.Text = "Send";
+            this.txt_Action.UseVisualStyleBackColor = true;
+            this.txt_Action.Click += new System.EventHandler(this.txt_Action_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label2.Location = new System.Drawing.Point(329, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(94, 40);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Port :";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // txtIP
+            // 
+            this.txtIP.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.txtIP.Font = new System.Drawing.Font("굴림", 16F);
+            this.txtIP.Location = new System.Drawing.Point(103, 3);
+            this.txtIP.Name = "txtIP";
+            this.txtIP.Size = new System.Drawing.Size(220, 32);
+            this.txtIP.TabIndex = 5;
+            this.txtIP.Text = "127.0.0.1";
+            this.txtIP.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // txtPort
+            // 
+            this.txtPort.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.txtPort.Font = new System.Drawing.Font("굴림", 16F);
+            this.txtPort.Location = new System.Drawing.Point(429, 3);
+            this.txtPort.Name = "txtPort";
+            this.txtPort.Size = new System.Drawing.Size(220, 32);
+            this.txtPort.TabIndex = 6;
+            this.txtPort.Text = "1883";
+            this.txtPort.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(652, 410);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Name = "Form1";
+            this.Text = "MQTTSender";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.TextBox txtSendText;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ListBox logbox;
+        private System.Windows.Forms.Button txt_Action;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtIP;
+        private System.Windows.Forms.TextBox txtPort;
+    }
+}
+
---
+++ mqtttester/Form1.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+using uPLibrary.Networking.M2Mqtt;
+using uPLibrary.Networking.M2Mqtt.Messages;
+
+namespace mqtttester
+{
+    public partial class Form1 : Form
+    {
+        public MqttClient mqttClient;
+        private static string CLIENT_ID = Guid.NewGuid().ToString() + "_MQTT_Sender";
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+     
+
+        /// <summary>
+        /// 구독한 Topic에 대한 메시지 수신 이벤트
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void MqttClient_MqttMsgPublishReceived(object sender, MqttMsgPublishEventArgs e)
+        {
+            Writelog("MQTT Received");
+            Writelog($"Topic : {e.Topic} - Message : {System.Text.Encoding.UTF8.GetString(e.Message)}");
+        }
+
+        private async void Writelog(string log)
+        {
+            if (logbox.InvokeRequired)
+            {
+                logbox.Invoke(new Action(() => logbox.Items.Insert(0, $"[{DateTime.Now}]{log}")));
+            }
+            else
+            {
+                logbox.Items.Insert(0, $"[{DateTime.Now}]{log}");
+            }
+        }
+
+        private void txt_Action_Click(object sender, EventArgs e)
+        {
+            //MQTT로 데이터 전송
+            Writelog($"MQTT Send : {txtSendText.Text}");
+            mqttClient.Publish($"MQTTTEST/{DateTime.Now.ToString("yyyyMMdd")}", System.Text.Encoding.UTF8.GetBytes(txtSendText.Text), MqttMsgBase.QOS_LEVEL_AT_MOST_ONCE, false);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            SubscribeStart();
+        }
+
+        /// <summary>
+        /// 원하는 MQTT Topic을 구독하는 메서드
+        /// </summary>
+        internal async void SubscribeStart()
+        {
+            //MQTT로 데이터 수집
+            try
+            {
+                mqttClient = new MqttClient($"{txtIP.Text}");
+                if (!mqttClient.IsConnected)
+                {
+                    mqttClient.MqttMsgPublishReceived += MqttClient_MqttMsgPublishReceived;
+                    mqttClient.Subscribe(new string[] { "MQTTTEST/#" }, new byte[] { MqttMsgBase.QOS_LEVEL_EXACTLY_ONCE }); //DMS Topic : event/c/data_collection_digit/#
+
+                    mqttClient.Connect($"{txtIP.Text}:{txtPort.Text}");
+                }
+                Writelog("MQTT Subscribe Start");
+
+            }
+            catch (Exception ex)
+            {
+                Writelog(ex.Message);
+            }
+
+        }
+    }
+}
